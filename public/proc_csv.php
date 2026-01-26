ff1fd1cd4a68ab26b8296bf954c882a3e809e0ae
LucusK <lucus.kim@tamu.edu>
2026-01-25 21:05:11 -0600

    it works, but need to clean up a bit

--- public/proc_csv.php
+++ public/proc_csv.php
@@ -59,8 +59,11 @@ function proc_csv ($filename, $delimiter, $quote, $columns_to_show) {
     //forgot cant use getcsv so need to implement using regex
     //refering to https://catswhocode.com/development/php-regex/ for function definitions
     //store all text in file
-    $page = @file_get_contents($filename);
-    if(!$page){ echo "cannot open file"; return;}
+    $file_handle = fopen($filename,"r");
+    if(!$file_handle){
+        echo "cannot open csv file";
+        return;
+    }
 
     //get the columns to show
     $columns = null;
@@ -77,8 +80,60 @@ function proc_csv ($filename, $delimiter, $quote, $columns_to_show) {
 
     //regex to find all entries
     //(?:"(?:[^"]|"")*"|[^,\r\n]*)(?=,|\r?\n|$)
-    $csv_regex = "(?:{$quote_reg}(?:[^{$quote_reg}]|{$quote_reg}{$quote_reg})*{$quote_reg}|[^{$delimiter_reg}\\r\\n]*)(?={delimiter_reg}|\r?\n|$)";
+    $csv_regex = "/(?:{$quote_reg}(?:[^{$quote_reg}]|{$quote_reg}{$quote_reg})*{$quote_reg}|[^{$delimiter_reg}\\r\\n]*)(?={$delimiter_reg}|\r?\n|$)/";
     
+    echo "<table border=\"1\">\n";
+
+    while(($line = fgets($file_handle))!==false){
+        //take the line and split into fields
+        $matches = [];
+        $test = preg_match_all($csv_regex, $line, $matches);
+        /*if($test === false){
+            echo "regex fail";
+            break;
+        }
+        if($test === 0){
+            continue;
+        }*/
+        //make a cell list for all matches
+        $cells = $matches[0];
+
+
+        $rows = array();
+        for($i = 0; $i < count($cells); $i++){
+            $temp = $cells[$i];
+            //appends temp string into rows
+            $rows[] = $temp;
+        }
+
+        echo "<tr>\n";
+        //for all
+        if($columns_to_show == "ALL"){
+            //add cells for each in the row
+            for($j = 0; $j < count($rows); $j++){
+                //cell = row at j column
+                echo "<td>". htmlspecialchars($rows[$j]) ."</td>\n";
+            }
+        }
+        //for specific columns
+        else{
+            for($k = 0; $k < count($columns); $k++){
+                //k_column is column number we want from curr row
+                //takes value if both is not negative and if witin row
+                $k_column = $columns[$k];
+                if ($k_column >= 0 && $k_column < count($rows)) {
+                    $value = $rows[$k_column];
+                } else {
+                    $value = "";
+                }
+                echo "<td>" . htmlspecialchars($value) . "</td>\n";
+            }
+        }
+        ///end row
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+    fclose($file_handle);
 
 }
 
