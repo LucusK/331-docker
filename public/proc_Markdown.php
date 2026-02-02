38e5f4e46cf7d376b15149bf7c7beebcb9a8106d
LucusK <lucus.kim@tamu.edu>
2026-02-01 21:52:04 -0600

    fully fleshed out the markdown

--- public/proc_Markdown.php
+++ public/proc_Markdown.php
@@ -20,7 +20,19 @@ function proc_markdown ($filename) {
     $orderedlist2 = false; #nested
 
     foreach($string_array as $curr_string){
-        #check headings, remember . is concatentation 
+        #check for blank line for paragraph break
+        if(trim($curr_string) === ""){
+            #check if already in paragraph
+            if($paragraph){
+                echo "</p>\n";
+                $paragraph = false;
+            }
+            continue;
+        }
+
+
+        #check headings, remember . is concatentation
+        #substr: string, starting, ending
         if(substr($curr_string,0,2)=== "# "){
             echo "<h1>" . helperStyling(substr($curr_string, 2)) . "</h1>\n";
             continue
@@ -33,7 +45,66 @@ function proc_markdown ($filename) {
             echo "<h3>" . helperStyling(substr($curr_string, 4)) . "</h3>\n";
             continue
         }
+
+
+
+        #unordered list  regex checks for non-charactrs then a * followed by the capture group for rest of line
+        if (preg_match('/^(\s*)\*\s+(.*)$/', $curr_string, $match)) { #list mode
+            if(!$unorderedlist){ #start top of unordered list
+                echo "<ul>\n";
+                $unorderedlist = true;
+            }
+            #see if nested, if first match is more 1 space
+            if(strlen($match[1]) >= 2) {
+                #start nested list
+                if(!$unorderedlist2){
+                    echo "<ul>\n";
+                    $unorderedlist = true;
+                }
+                echo "<li>" . helperStyling($match[2]) . "</li>\n";
+            }
+            else{ #not nested
+                if($unorderedlist2){ #end nest
+                    echo "</ul>\n";
+                    $unorderedlist2 = false;
+                }
+                echo "<li>" . helperStyling($match[2]) . "</li>\n";
+            }           
+            continue;
+        }
+
+        #ordered list, similar logic to unordered
+        #regex check if starts with 1.
+        if(preg_match('/^\s*1\.\s+(.*)$/', $curr_string, $match)) {
+            if (!$orderedlist){
+                echo "<ol>\n";
+                $orderedlist = true;
+            }
+            echo "<li>" . helperStyling($match[1]) . "</li>\n";
+            continue;
+        }
+        #nested ordered, regex check if starts with -
+        if(preg_match('/^\s*-\s+(.*)$/', $curr_string, $match)) {
+            if(!$orderedlist2){
+                echo "<ol>\n";
+                $orderedlist2 = true;
+            }
+            echo "<li>" . helperStyling($match[1]) . "</li>\n";
+            continue;
+        }
+
+        #make ending a new paragraph
+        if(!$paragraph){
+            echo "<p>";
+            $paragraph = true;
+        }
+        echo helperStyling($curr_string) . " ";
     }
+    if ($paragraph) echo "</p>\n";
+    if ($unorderedlist2) echo "</ul>\n";
+    if ($unorderedlist) echo "</ul>\n";
+    if ($orderedlist2) echo "</ol>\n";
+    if ($orderedlist) echo "</ol>\n";
 }
 
 function helperStyling($in){
