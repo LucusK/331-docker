b07c9799df1c5a8b5418ae38cc806a3ee89984ff
LucusK <lucus.kim@tamu.edu>
2026-02-02 01:40:14 -0600

    why dont you work

--- public/search.php
+++ public/search.php
@@ -7,7 +7,8 @@
 function search($string)  {
     #first sanitize, only care about letters & numbers
     $query = trim($string); #trim found in php.net, removes whitespace
-    $query = preg_replace('/[^a-zA-Z0-9]/','',$query);
+    $query = preg_replace('/[^a-zA-Z0-9 ]/','',$query);
+    echo "query: " . $query . "<br>\n";
 
     #find the local web directory
     #php.net uses $_SERVER which has 'DOCUMENT_ROOT'
@@ -20,15 +21,33 @@ function search($string)  {
     $php_files = glob($web_root_dir . "/*.php");
     $html_files = glob($web_root_dir . "/*.html");
 
-    #converts its file path into a URL on localhost:5555
+    $all_files = array_merge($php_files, $html_files);
 
-    #loads the page through the web server so that any PHP code is executed and the final HTML output is retrieved
+    foreach($all_files as $curr_file){
+        #converts its file path into a URL on localhost:5555
+        $url = "http://localhost:5555" . substr($curr_file, strlen($web_root_dir));
+        echo "trying: $url<br>";
 
-    #search on this generated HTML to check whether the search keyword appears
-
+        #loads the page through the web server so that any PHP code is executed and the final HTML output is retrieved
+        #turn into html using file_get_contents
+        # php.net says this reads entire file into string. it also would generate the page in the website first before sending it over
+        $file_string = @file_get_contents($url); 
+        if($file_string === false){
+            continue;
+        } #we basically store all contents in a string
+        #search on this generated HTML to check whether the search keyword appears
+        if(str_contains($file_string, $query)){ #found from php.net: Determine if a string contains a given substring
+            $matches[] = $url; #found this is same as array_push, in definition on php.net
+        }
+    }
     #outputs an ordered list of hyperlinks to all pages that contain the search
-
-
+    echo "matches: " . count($matches) . "<br>\n";
+    echo "<ol>";
+    foreach ($matches as $match) {
+        echo "<li><a href=\"$match\">$match</a></li>";
+    }
+    echo "</ol>";
+    
 
 }
 
